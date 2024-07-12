a105e67619c5483c5cd2badde604f26b5a2d0459
Fabio Andrade <135459469+FabioBarAnd@users.noreply.github.com>
2024-07-12 15:12:34 -0300

    Add files via upload

---
+++ TiposPrimitivos/src/main/java/com/mycompany/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiposprimitivos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        /*Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = teclado.nextLine();
+        System.out.print("Digite sua nota: ");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota de %s é %.1f", nome, nota);*/
+        
+        /*int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);*/
+        
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(valor);
+    }
+}
